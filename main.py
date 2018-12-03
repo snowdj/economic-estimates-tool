891e79744dab2356a01a889d2f7524157a2621dc
Max U <maxunsted@gmail.com>
2018-12-03 22:10:03 +0000

    add time series chart and map - work in progress

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, flash, redirect, render_template, request, session, abort, make_response, url_for
+
+app = Flask(__name__)
+
+# appened static content with version number to overcome caching
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+@app.route('/')
+def index():
+    """Render home page."""
+    return render_template('index.html')  # we can render templates as usual
+
+if __name__ == '__main__':
+    app.run(debug=True)
